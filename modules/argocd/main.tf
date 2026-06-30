cb72a9cff8a43a6bd9d7e94a5891b70480eda5dc
Ido <Idoel273@gmail.com>
2026-06-30 19:10:03 +0300

    feat: add ArgoCD via Helm with App of Apps pattern, fix ECR force_delete

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
@@ -5,4 +14,31 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   create_namespace = true
   version          = "~> 7.0"
+}
+
+resource "kubectl_manifest" "root_app" {
+  yaml_body = <<-YAML
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      name: root-app
+      namespace: argocd
+    spec:
+      project: default
+      source:
+        repoURL: https://github.com/ido273/warehouse-gitops.git
+        targetRevision: master
+        path: apps
+        directory:
+          recurse: true
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: argocd
+      syncPolicy:
+        automated:
+          prune: true
+          selfHeal: true
+  YAML
+
+  depends_on = [helm_release.argocd]
 }
--- providers.tf
+++ providers.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
   }
   required_version = ">= 1.0"
 }
@@ -25,5 +29,16 @@ provider "helm" {
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.region]
     }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.region]
   }
 }
