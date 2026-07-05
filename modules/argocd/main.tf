c554ae699a2f09c7e2f2327e892b78360e4a9923
Ido <Idoel273@gmail.com>
2026-07-05 14:08:52 +0300

    feat: add S3 bucket module for image storage

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -72,3 +72,11 @@ resource "kubernetes_secret" "mysql" {
   type       = "Opaque"
   depends_on = [kubernetes_namespace.warehouse]
 }
+resource "helm_release" "nginx_ingress" {
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  depends_on       = [helm_release.argocd]
+}
